a9301b3d4343421654506d5ea9ba7b96db97d1d6
yanis-ouledmoussa <yanis@voulez-vous.studio>
2026-03-12 12:06:28 +0100

    newtonsoft json plugin usage update

--- Editor/DependencyLoader.cs
+++ Editor/DependencyLoader.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Unity.Plastic.Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Linq;
 using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
